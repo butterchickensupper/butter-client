187961af78a414633d6e743b423565f82ede805d
Butter Chicken <butterchickensupper@gmail.com>
2022-03-22 03:26:26 +0000

    simplify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UpdateMenuComponent } from './admin/update-menu/update-menu.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'update-menu', component: UpdateMenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { UpdateMenuComponent } from './admin/update-menu/update-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PageNotFoundComponent, SidenavComponent, ToolbarComponent, UpdateMenuComponent],
+  declarations: [AppComponent, LoginComponent, PageNotFoundComponent, SidenavComponent, ToolbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
