1d3e94a8602555ebe106898810c8ac11bce2cfd5
Butter Chicken <butterchickensupper@gmail.com>
2022-03-23 03:16:47 +0000

    misc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'menu', component: MenuComponent },
+  { path: 'order-history', component: OrderHistoryComponent },
   { path: '', redirectTo: '/menu', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
