f19939577c3a55ee3db922f4acdac0095693f5ae
Butter Chicken <butterchickensupper@gmail.com>
2022-03-28 00:45:29 +0000

    maybe

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,5 +1,11 @@
+import { MenuItem } from '../models/menu';
 import { MenuOrder } from '../models/order';
 
 export interface IAppState {
   readonly order: MenuOrder[];
+}
+
+export interface IConfig {
+  orders: MenuOrder[];
+  items: MenuItem[];
 }
