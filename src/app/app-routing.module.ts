f859fd01a014b226efa660f46e24d22a391038d4
Butter Chicken <butterchickensupper@gmail.com>
2022-03-29 02:32:26 +0000

    moving on up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AboutComponent } from './core/about/about.component';
 import { CartComponent } from './menu/cart/cart.component';
 import { EditItemComponent } from './editor/edit-item/edit-item.component';
 import { EditorComponent } from './editor/editor.component';
 import { MenuComponent } from './menu/menu.component';
-import { OrderHistoryComponent } from './menu/order-history/order-history.component';
+import { NgModule } from '@angular/core';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { ProfileComponent } from './core/profile/profile.component';
-import { OrderDashboardComponent } from './menu/order-dashboard/order-dashboard.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
