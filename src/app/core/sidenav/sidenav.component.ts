5dde991f76647b034c7a4bd9c671b18debd4c4c4
Butter Chicken <butterchickensupper@gmail.com>
2022-04-02 15:42:43 +0000

    fix scrolling

--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
-  constructor() {}
+export class SidenavComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      const el = document.querySelector('.mat-sidenav-content');
+      if (!el) {
+        return;
+      }
+      el.scrollTop = 0;
+    });
+  }
 
   @Input()
   public opened: boolean = false;
