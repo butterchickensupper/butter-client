165d8ffd6a499faab1e361bec0ca3a7f1a610b45
Butter Chicken <butterchickensupper@gmail.com>
2022-04-05 02:15:26 +0000

    hide edit

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -13,17 +13,19 @@ export class MenuOrder {
 }
 
 export class Order {
+  public id?: string;
   public items: MenuOrder[] = [];
   public name!: string;
   public address!: string;
   public date: Date;
   public total?: number; // TODO: needs to be set on orderSubmit
 
-  constructor(args: { name: string; address: string; items: MenuOrder[]; date: Date; total?: number }) {
+  constructor(args: { name: string; address: string; items: MenuOrder[]; date: Date; total?: number; id?: string }) {
     this.name = args.name;
     this.address = args.address;
     this.items = args.items;
     this.date = args.date;
     this.total = args.total;
+    this.id = args.id;
   }
 }
