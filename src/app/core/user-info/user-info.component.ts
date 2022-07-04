f6779c33ffbb2dcfba19db0e063433e4a2a40e05
Butter Chicken <butterchickensupper@gmail.com>
2022-07-04 14:04:19 +0000

    remove gmaps

--- src/app/core/user-info/user-info.component.ts
+++ src/app/core/user-info/user-info.component.ts
@@ -1,46 +1,27 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-user-info',
-  templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.scss']
+    selector: 'app-user-info',
+    templateUrl: './user-info.component.html',
+    styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent implements AfterViewInit {
-  private autocomplete!: google.maps.places.Autocomplete;
+export class UserInfoComponent {
+    @ViewChild('address')
+    public address!: ElementRef<HTMLInputElement>;
 
-  @ViewChild('address')
-  public address!: ElementRef<HTMLInputElement>;
-
-  public form = this.fb.group({
-    name: ['', [Validators.required]],
-    address: ['', [Validators.required]],
-    phone: ['', [Validators.required]]
-  });
-
-  public get name(): string {
-    return this.form.get('name')?.value;
-  }
-
-  public get addressResult(): string {
-    return this.form.get('address')?.value;
-  }
-
-  constructor(public fb: FormBuilder) {}
-
-  ngAfterViewInit(): void {
-    this.autocomplete = new google.maps.places.Autocomplete(this.address.nativeElement, {
-      componentRestrictions: { country: ['us'] },
-      fields: ['address_components'],
-      types: ['address']
+    public form = this.fb.group({
+        name: ['', []],
+        phone: ['', []],
     });
-    this.autocomplete.addListener('place_changed', this.updateAddress);
-  }
 
-  private updateAddress(): void {
-    const place = this.autocomplete.getPlace();
-    console.log(place);
-    const b = place.address_components as google.maps.GeocoderAddressComponent[];
-    console.log(b);
-  }
+    public get name(): string {
+        return this.form.get('name')?.value;
+    }
+
+    public get addressResult(): string {
+        return this.form.get('address')?.value;
+    }
+
+    constructor(public fb: FormBuilder) {}
 }
