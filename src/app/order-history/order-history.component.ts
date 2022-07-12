48c49bc270fba992a77e886c997eea4709967d96
Butter Chicken <butterchickensupper@gmail.com>
2022-07-12 18:19:40 +0000

    calc total

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 
 import { Order } from '../models/order';
-import { OrderHistoryRequest } from '../models/order-history-request';
+import { AuthService } from '../services/auth/auth.service';
 import { LoadingService } from '../services/loading/loading.service';
 import { OrderService } from '../services/order/order.service';
 
@@ -12,16 +12,27 @@ import { OrderService } from '../services/order/order.service';
     styleUrls: ['./order-history.component.scss'],
 })
 export class OrderHistoryComponent {
-    public orders$: Observable<Order[]>;
+    public orders$: Observable<Order[]> = of([]);
     public step = 0;
     public panelOpenState = false;
     public displayedColumns: string[] = ['quantity', 'name', 'price'];
 
-    constructor(private orderServce: OrderService, private loadingService: LoadingService) {
+    constructor(private orderServce: OrderService, private loadingService: LoadingService, private authService: AuthService) {
+        // TODO: uncomment
+        // if (!this.authService.currentUser) return;
         setTimeout(() => this.loadingService.show(), 0);
-        this.orders$ = this.orderServce
-            .getHistory(new OrderHistoryRequest({ userId: 'test123' }))
-            .pipe(tap(() => setTimeout(() => this.loadingService.hide(), 0)));
+        this.orders$ = this.orderServce.getHistory().pipe(
+            map((orders) => {
+                orders.map((order) => {
+                    order.total = 0;
+                    for (const item of order.items) {
+                        order.total += item.quantity * item.item.price;
+                    }
+                });
+                return orders;
+            }),
+            tap(() => setTimeout(() => this.loadingService.hide(), 0))
+        );
     }
 
     public setStep(index: number) {
